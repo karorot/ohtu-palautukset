19117b2a51a60ffe2df70e8372b1e8d14a28e91d
karorot <174889933+karorot@users.noreply.github.com>
2025-11-08 09:02:45 +0200

    Improve player listing with user input and better UI
    Improve player listing by getting user input for season and nationality
    of the player data that is shown. Add also light graphic UI with Rich.

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,15 +1,58 @@
+from rich.table import Table, Column
+from rich.console import Console
+from rich.theme import Theme
+from rich.prompt import Prompt
 from player_reader import PlayerReader
 from player_stats import PlayerStats
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    custom_theme = Theme({"info": "bold magenta", "warning": "red"})
+    console = Console(theme=custom_theme)
+
+    console.print("Player statistics available for seasons 2018-2026", style="info")
+    console.print("Quit at any time with 'q'", style="warning")
+    season = Prompt.ask("Choose season, e.g. 2024-25").strip()
+
+    if season.lower() == 'q':
+        return
+
+    url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
     reader = PlayerReader(url)
     stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
 
-    print("Players from FIN:\n")
-    for player in players:
-        print(player)
+    console.print("Use the 3-letter country codes to list players (e.g. FIN, SWE, CAN)",
+                    style="info")
+
+    while True:
+        nat = Prompt.ask("Choose nationality").strip()
+
+        if nat.lower() == "q":
+            break
+
+        players = stats.top_scorers_by_nationality(nat.upper())
+
+        table = Table(
+            Column(header="Player", justify="left", style="cyan", no_wrap=True),
+            Column(header="Teams", justify="left", style="magenta"),
+            Column(header="Goals", justify="right", style="green"),
+            Column(header="Assists", justify="right", style="green"),
+            Column(header="Points", justify="right", style="green"),
+            title=f"Players from {nat.upper()} in {season}"
+        )
+
+        for player in players:
+            table.add_row(
+                player.name,
+                player.team,
+                str(player.goals),
+                str(player.assists),
+                str(player.goals + player.assists)
+            )
+
+        if table.rows:
+            console.print(table)
+        else:
+            print("No data available.\n")
 
 if __name__ == "__main__":
     main()

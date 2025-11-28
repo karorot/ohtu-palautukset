b0b4f85550c28e115bcb13cb1a18b57e0b58ccd8
karorot <174889933+karorot@users.noreply.github.com>
2025-11-28 21:47:24 +0200

    Replace magic numbers with enums

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,9 +1,18 @@
+from enum import IntEnum
+
+class Score(IntEnum):
+    LOVE = 0
+    FIFTEEN = 1
+    THIRTY = 2
+    FORTY = 3
+    ADVANTAGE = 4
+
 class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.m_score1 = Score.LOVE
+        self.m_score2 = Score.LOVE
 
     def won_point(self, player_name):
         # how to have cleaner dependency on name
@@ -16,17 +25,17 @@ class TennisGame:
         if self.m_score1 == self.m_score2:
             return self.call_even_score()
 
-        if self.m_score1 >= 4 or self.m_score2 >= 4:
+        if self.m_score1 >= Score.ADVANTAGE or self.m_score2 >= Score.ADVANTAGE:
             return self.call_winning_score()
 
         return self.call_score()
 
     def call_even_score(self):
-        if self.m_score1 == 0:
+        if self.m_score1 == Score.LOVE:
             return "Love-All"
-        if self.m_score1 == 1:
+        if self.m_score1 == Score.FIFTEEN:
             return "Fifteen-All"
-        if self.m_score1 == 2:
+        if self.m_score1 == Score.THIRTY:
             return "Thirty-All"
         return "Deuce"
 
@@ -52,16 +61,13 @@ class TennisGame:
                 score = score + "-"
                 temp_score = self.m_score2
 
-            if temp_score == 0:
+            if temp_score == Score.LOVE:
                 score = score + "Love"
-            elif temp_score == 1:
+            elif temp_score == Score.FIFTEEN:
                 score = score + "Fifteen"
-            elif temp_score == 2:
+            elif temp_score == Score.THIRTY:
                 score = score + "Thirty"
-            elif temp_score == 3:
+            elif temp_score == Score.FORTY:
                 score = score + "Forty"
 
         return score
-
-    def score_call(self, points):
-        pass

6ee21ea818f9567313521e9e5b7f1d5a5412b809
karorot <174889933+karorot@users.noreply.github.com>
2025-11-28 21:27:22 +0200

    Split score calling to smaller methods
    Split score calling into three smaller methods: even score, winning score
    and just score.

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -6,50 +6,62 @@ class TennisGame:
         self.m_score2 = 0
 
     def won_point(self, player_name):
+        # how to have cleaner dependency on name
         if player_name == "player1":
             self.m_score1 = self.m_score1 + 1
         else:
             self.m_score2 = self.m_score2 + 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
-
         if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+            return self.call_even_score()
 
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+        if self.m_score1 >= 4 or self.m_score2 >= 4:
+            return self.call_winning_score()
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+        return self.call_score()
+
+    def call_even_score(self):
+        if self.m_score1 == 0:
+            return "Love-All"
+        if self.m_score1 == 1:
+            return "Fifteen-All"
+        if self.m_score1 == 2:
+            return "Thirty-All"
+        return "Deuce"
+
+    def call_winning_score(self):
+        minus_result = self.m_score1 - self. m_score2
+
+        if minus_result == 1:
+            return "Advantage player1"
+        if minus_result == -1:
+            return "Advantage player2"
+        if minus_result >= 2:
+            return "Win for player1"
+        return "Win for player2"
+
+    def call_score(self):
+        temp_score = 0
+        score = ""
+
+        for i in range(1, 3):
+            if i == 1:
+                temp_score = self.m_score1
+            else:
+                score = score + "-"
+                temp_score = self.m_score2
+
+            if temp_score == 0:
+                score = score + "Love"
+            elif temp_score == 1:
+                score = score + "Fifteen"
+            elif temp_score == 2:
+                score = score + "Thirty"
+            elif temp_score == 3:
+                score = score + "Forty"
 
         return score
+
+    def score_call(self, points):
+        pass

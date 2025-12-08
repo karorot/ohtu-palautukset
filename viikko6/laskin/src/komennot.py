2ed433c720a9611a501c9078f2ecc4aac6d44bb5
karorot <174889933+karorot@users.noreply.github.com>
2025-12-08 08:53:02 +0200

    Refactor Laskin and Komento objects

---
+++ viikko6/laskin/src/komennot.py
@@ -0,0 +1,32 @@
+class Summa:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        arvo = self._lue_syote()
+        self._sovelluslogiikka.plus(arvo)
+
+class Erotus:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        arvo = self._lue_syote()
+        self._sovelluslogiikka.miinus(arvo)
+
+class Nollaus:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        self._sovelluslogiikka.nollaa()
+
+class Kumoa:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        pass
+
+    def suorita(self):
+        pass
